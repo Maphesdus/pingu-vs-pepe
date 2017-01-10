57b5a41b83a50899c294db2bd32ddd9b3a8bfad5
David Dubois <dexcelstraun7@gmail.com>
2017-01-10 11:33:26 -0500

    Added particleSystems standard asset

---
+++ Pingu vs. Pepe/Assets/Code/Lifetime.cs
@@ -0,0 +1,24 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Simple script to kill things after a certain number of seconds
+/// </summary>
+[AddComponentMenu("Utility/Lifetime")]
+public class Lifetime : MonoBehaviour {
+    [SerializeField]
+    private float lifetime;
+
+    [SerializeField]
+    private GameObject replacement;
+
+    IEnumerator Start() {
+        yield return new WaitForSeconds(lifetime);
+        if(replacement) {
+            Instantiate(replacement, transform.position, transform.rotation);
+        }
+        Destroy(this);
+    }
+}
---
+++ Pingu vs. Pepe/Assets/Code/Weapons/Grenades/GrenadeThrower.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Allows a character to throw a grenade
+/// </summary>
+public class GrenadeThrower : MonoBehaviour {
+    [SerializeField]
+    private GameObject grenadePrefab;
+
+    [SerializeField]
+    private float throwForce;
+
+	public void throwGrenade(Vector3 startPos, Vector3 direction) {
+        GameObject grenade = Instantiate(grenadePrefab, startPos, Quaternion.identity);
+        Rigidbody grenadeRigidbody = grenade.GetComponent<Rigidbody>();
+        grenadeRigidbody.AddForce(throwForce * direction);
+    }
+}
