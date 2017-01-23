58b547065300a3878e6ef82a20090126a7fa8b43
FF-syk <indyjonesjr@hotmail.com>
2017-01-23 02:51:59 -0500

    Basic Menus
    Starting menu, placeholder options menu, and pause menu that can be accessed by "P".

---
+++ Pingu vs. Pepe/Assets/GUI/PauseMenuFunctions.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenuFunctions : MonoBehaviour {
+    bool paused;
+    bool gameStart = false;
+    public GameObject pauseMenu;
+
+    void Start()
+    {
+        //Game begins paused.
+        Time.timeScale = 0;
+        paused = true;
+        gameStart = false;
+    }
+
+    public void quitGame()
+    {
+        Application.Quit();
+        Debug.Log("QUIT!");
+    }
+
+    public void startGame()
+    {
+        unPauseGame();
+        gameStart = true;
+    }
+
+    public void endGame()
+    {
+        paused = true;
+        Time.timeScale = 0;
+        gameStart = false;
+    }
+
+    public void pauseGame()
+    {
+        paused = true;
+        Time.timeScale = 0;
+        pauseMenu.SetActive(true);
+    }
+
+    public void unPauseGame()
+    {
+        paused = false;
+        Time.timeScale = 1;
+        pauseMenu.SetActive(false);
+    }
+    
+	// Update is called once per frame
+	void Update () {
+        if (gameStart && Input.GetKeyDown(KeyCode.P))
+        {
+            if (paused) unPauseGame();
+            else pauseGame();
+        }
+	}
+}
---
+++ Pingu vs. Pepe/Assets/GUI/PauseMenuListener.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenuListener : MonoBehaviour {
+
+    void pauseGame()
+    {
+        Debug.Log("Got it");
+        gameObject.SetActive(true);
+    }
+
+    void unPauseGame()
+    {
+        gameObject.SetActive(false);
+    }
+
+}
